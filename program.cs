6328948aa86b62ec5a3cd69fcbc2530bb8183657
Ангелина Константинавичюс <Konstantinavi_AD_25@student.grsu.by>
2026-09-14 13:16:46 +0300

    добавила словарь цветов

--- program.cs
+++ program.cs
@@ -5,3 +5,32 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+namespace laba1
+{
+    class Program
+    {
+        static Dictionary<string, Color> ColorMap = new(StringComparer.OrdinalIgnoreCase)
+        {
+            { "красн", Color.Red },
+            { "ал", Color.Crimson },
+            { "багр", Color.DarkRed },
+            { "зелен", Color.Green },
+            { "изумруд", Color.MediumSeaGreen },
+            { "малахит", Color.MediumSeaGreen },
+            { "син", Color.Blue },
+            { "голуб", Color.LightBlue },
+            { "лазур", Color.LightSkyBlue },
+            { "ультрамарин", Color.Blue },
+            { "желт", Color.Yellow },
+            { "золот", Color.Gold },
+            { "лимон", Color.LemonChiffon },
+            { "бел", Color.White },
+            { "черн", Color.Black },
+            { "сер", Color.Gray },
+            { "фиолетов", Color.Purple },
+            { "лилов", Color.Purple },
+            { "оранжев", Color.Orange },
+            { "коричнев", Color.Brown },
+            { "розов", Color.Pink },
+            { "бирюз", Color.Turquoise },
+        };
